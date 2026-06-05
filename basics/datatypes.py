91aadc4ffa22e02f0ed6c4bcf1959f3ccb76845a
SmartSakshi13 <bhosalesakshi119@gmail.com>
2026-06-05 15:09:27 +0000

    Basics of python Programming

---
+++ basics/datatypes.py
@@ -0,0 +1,10 @@
+age = 21
+height = 5.4
+name = "Sakshi"
+is_student = True
+
+print(f"name : {name} , type : {type(name)}")
+print(f"height : {height} , type : {type(height)}")
+print(f"age : {age} , type : {type(age)}")
+print(f"is_student : {is_student} , type : {type(is_student)}")
+
---
+++ basics/variables.py
@@ -0,0 +1,13 @@
+# A variable stores data
+
+name = "Sakshi"
+age = 21
+
+'''
+here:
+	name stores text
+	age stores a number
+'''
+
+print(name)
+print(age)
