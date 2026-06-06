04cb99ac2ece0122202a3f349dc6df356348c8c1
SmartSakshi13 <bhosalesakshi119@gmail.com>
2026-06-06 16:12:56 +0000

    Condition statements

---
+++ conditions/example.py
@@ -0,0 +1,8 @@
+distance = 10
+
+if distance < 20:
+	print("Stop")
+
+else :
+	print("Move Forward")
+
---
+++ conditions/ifelifelse.py
@@ -0,0 +1,11 @@
+marks = 75
+
+if marks >= 90:
+	print('A')
+
+elif marks >= 75:
+	print('B')
+
+else :
+	print('C')
+
