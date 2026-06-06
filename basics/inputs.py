c6bbcbf80407fc20cad5d5233a4d6e71c2930d5b
SmartSakshi13 <bhosalesakshi119@gmail.com>
2026-06-06 15:54:08 +0000

    Input and operator programs

---
+++ basics/inputs.py
@@ -0,0 +1,8 @@
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+
+# int(input()) it is the conversion of string input into integer
+
+print(name)
+print(age)
+
---
+++ basics/operators.py
@@ -0,0 +1,18 @@
+a = 10 
+b = 5
+
+print("=== Mathematical Operators ===")
+print(f"a:{a},b:{b}")
+print(f"Addition : {a+b}")
+print(f"Substraction : {a-b}")
+print(f"Division : {a/b}")
+print(f"Multiplication : {a*b}")
+
+print("\n\n==== Comaparision Operator ====")
+print(a>b)
+print(a<b)
+print(a==b)
+
+
+
+
